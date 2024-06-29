4c7d2625af3b9ed670b9053d038fc629ef13ff6c
AlexWoIf <alexey.barannikov@gmail.com>
2024-06-29 21:32:08 +0300

    read words from dict file

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import asyncio
 
+import aiofiles
 import aiohttp
 import pymorphy2
 
 import adapters.inosmi_ru as inosmi
 import text_tools
+
+
+async def read_words_from_file(file_path):
+    async with aiofiles.open(file_path, mode='rb') as word_file:
+        words = []
+        async for line in word_file:
+            # queues['messages_queue'].put_nowait(line.decode('cp1251').strip())
+            words.append(line.decode('utf8').strip())
+    return words
+
 
 async def fetch(session, url):
     async with session.get(url) as response:
@@ -16,21 +27,13 @@ async def main():
     async with aiohttp.ClientSession() as session:
         html = await fetch(session, 'https://inosmi.ru/20240628/drevnie-lyudi-269349491.html')
         clean_plaintext = inosmi.sanitize(html, plaintext=True)
-        # print(clean_plaintext)
 
         morph = pymorphy2.MorphAnalyzer()
         article_words = text_tools.split_by_words(morph, clean_plaintext)
-        charged_words = ['срочно', 'весьма', 'эксклюзивный']
+
+        charged_words = await read_words_from_file('./negative_words.txt')
         raiting = text_tools.calculate_jaundice_rate(article_words, charged_words)
 
-        words_raiting = {}
-        for word in article_words:
-            words_raiting[word] = words_raiting.get(word, 0) + 1
-        # sorted_raiting = {key: value for key, value in 
-        #                   sorted(words_raiting.items(),
-        #                          key=lambda item: item[1])}
-        for word in charged_words:
-            print(f'{word}: {words_raiting.get(word, 0)}')
         print(f'Рейтинг: {raiting}\nСлов в статье: {len(article_words)}')
 
 asyncio.run(main())

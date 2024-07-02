7388e225bb7af9e1625c17acd0488c01379045f8
AlexWoIf <alexey.barannikov@gmail.com>
2024-07-02 22:30:24 +0300

    add tests for process_article

--- server.py
+++ server.py
@@ -2,34 +2,38 @@ import pymorphy2
 from aiohttp import web
 from anyio import create_task_group
 
-import main
+import urls_handler
+
+
+MAX_URLS = 10
 
 
 def prepare_response(articles_stat):
     response = []
-    for url, (status, rating, words_count, duration) in articles_stat.items():
+    for url, (status, rating, words_count, _) in articles_stat.items():
         response.append({'status': status.value, 'url': url, 'score': rating,
                          'words_count': words_count})
     return response
 
 
 async def handle(request, morph):
-    urls = request.rel_url.query.get('urls')
+    urls_text = request.rel_url.query.get('urls')
+    urls = urls_text.split(',') if urls_text else []
+    if len(urls) > MAX_URLS:
+        print(f'{len(urls)=}')
+        raise web.HTTPBadRequest(text='{"error": "too many urls in request, '
+                                 f'should be {MAX_URLS} or less"}}',
+                                 content_type="application/json")
     articles_stat = {}
     async with create_task_group() as tg:
-        for article in urls.split(',') if urls else []:
-            tg.start_soon(main.process_article, article, articles_stat, morph)
+        for article in urls:
+            tg.start_soon(urls_handler.process_article, article, articles_stat, morph)
     response = prepare_response(articles_stat)
     print(response)
     return web.json_response(response)
-    # return web.json_response({'urls': urls.split(',') if urls else []})
 
 
 if __name__ == '__main__':
-    # logging.basicConfig(
-    #     format='%(levelname)s:%(filename)s:[%(asctime)s] %(message)s',
-    #     level=logging.DEBUG,
-    # )
     morph = pymorphy2.MorphAnalyzer()
 
     app = web.Application()
--- urls_handler.py
+++ urls_handler.py
@@ -16,6 +16,7 @@ import text_tools
 from adapters.exceptions import ArticleNotFound
 
 
+CHARGED_WORDS_FILE = './negative_words.txt'
 TEST_ARCTICLES = ['https://inosmi.ru/20240628/drevnie-lyudi-269349491.html',
                   'https://inosmi.ru/20240629/kulisy-269369858.html',
                   'https://inosmi.ru/20240629/assanzh-269370889.html',
@@ -24,7 +25,7 @@ TEST_ARCTICLES = ['https://inosmi.ru/20240628/drevnie-lyudi-269349491.html',
                   'https://inosmi.ru/20240629/byudzhet-269368449.html',
                   'https://inosmi.ru/not/exist.html',
                   'https://lenta.ru/brief/2021/08/26/afg_terror/',
-                  'big_text', ]
+                  'big_text_test', ]
 TIMEOUT = 7
 
 
@@ -33,6 +34,9 @@ class ProcessingStatus(Enum):
     FETCH_ERROR = 'FETCH_ERROR'
     PARSING_ERROR = 'PARSING_ERROR'
     TIMEOUT = 'TIMEOUT'
+
+    def __str__(self):
+        return str(self.value)
 
 
 async def read_words_from_file(file_path):
@@ -55,11 +59,11 @@ async def use_timer(article, articles_stat):
     try:
         yield start_time
     finally:
-        status, rating, word_count, duration = articles_stat.get(article,
-                                                                 [None]*4)
+        status, rating, words_count, duration = articles_stat.get(article,
+                                                                  [None]*4)
         if duration is None:
             duration = time.monotonic() - start_time
-            articles_stat[article] = [status, rating, word_count, duration]
+            articles_stat[article] = [status, rating, words_count, duration]
 
 
 async def process_article(article, articles_stat, morph):
@@ -67,7 +71,7 @@ async def process_article(article, articles_stat, morph):
         async with use_timer(article, articles_stat) as start_time:
             try:
                 async with timeout(TIMEOUT):
-                    if article == 'big_text':
+                    if article == 'big_text_test':
                         clean_plaintext = big_text.big_text
                     else:
                         html = await fetch(session, article)
@@ -77,14 +81,14 @@ async def process_article(article, articles_stat, morph):
                         morph, clean_plaintext)
 
                     charged_words = await read_words_from_file(
-                        './negative_words.txt')
+                        CHARGED_WORDS_FILE)
                     rating = text_tools.calculate_jaundice_rate(
                         article_words, charged_words)
                     status = ProcessingStatus.OK
-                    word_count = len(article_words)
+                    words_count = len(article_words)
                     duration = time.monotonic() - start_time
                     articles_stat[article] = \
-                        [status, rating, word_count, duration]
+                        [status, rating, words_count, duration]
             except aiohttp.ClientResponseError:
                 status = ProcessingStatus.FETCH_ERROR
                 articles_stat[article] = [status, None, None, None]
@@ -110,10 +114,33 @@ async def main():
     for article, stats in articles_stat.items():
         print(f'\nURL: {article:}')
         status, rating, words, duration = stats
-        print(f'Статус: {status.value}')
+        print(f'Статус: {status}')
         print(f'Рейтинг: {rating}')
         print(f'Слов в статье: {words}')
         logging.info(f'Анализ закончен за {duration} сек.')
+
+
+def test_process_article():
+    articles_stat = {}
+    morph = pymorphy2.MorphAnalyzer()
+
+    asyncio.run(process_article('https://inosmi.ru/not/exist.html', articles_stat, morph))
+    status, rating, words, _  = articles_stat['https://inosmi.ru/not/exist.html']
+    assert (status, rating, words) == (ProcessingStatus.FETCH_ERROR, None, None)
+
+    asyncio.run(process_article('https://lenta.ru/brief/2021/08/26/afg_terror/', articles_stat, morph))
+    status, rating, words, _  = articles_stat['https://lenta.ru/brief/2021/08/26/afg_terror/']
+    assert (status, rating, words) == (ProcessingStatus.PARSING_ERROR, None, None)
+
+    asyncio.run(process_article('https://inosmi.ru/20240629/assanzh-269370889.html', articles_stat, morph))
+    status, rating, words, duration = articles_stat['https://inosmi.ru/20240629/assanzh-269370889.html']
+    assert (status, rating, words) == (ProcessingStatus.OK, 1.46, 685)
+    assert duration < TIMEOUT
+
+    asyncio.run(process_article('big_text_test', articles_stat, morph))
+    status, rating, words, duration = articles_stat['big_text_test']
+    assert (status, rating, words) == (ProcessingStatus.TIMEOUT, None, None)
+    assert duration <= TIMEOUT
 
 
 if __name__ == '__main__':
